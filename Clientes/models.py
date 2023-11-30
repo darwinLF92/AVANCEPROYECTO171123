573a19aea2f8e14f2aa2ce0ecf6338fba9db3087
darwinLF92 <145072019+darwinLF92@users.noreply.github.com>
2023-11-30 11:08:33 -0600

    cambio models cliente

--- Clientes/models.py
+++ Clientes/models.py
@@ -3,11 +3,11 @@ from django.utils import timezone
 
 
 class Cliente(models.Model):
-    nit = models.CharField(max_length=20, unique=True, verbose_name='NIT')
+    nit = models.CharField(max_length=20, verbose_name='NIT')
     nombre = models.CharField(max_length=100, verbose_name='Nombre')
     direccion = models.CharField(max_length=200, verbose_name='Dirección')
     telefono = models.CharField(max_length=20, verbose_name='Teléfono')
-    correo = models.EmailField(max_length=100, verbose_name='Correo Electrónico')
+    correo = models.EmailField(max_length=100, verbose_name='Correo Electrónico', blank=True)
     limitecredito = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Límite de Crédito')
     saldo = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     fecha_creacion = models.DateTimeField(default=timezone.now, verbose_name='Fecha de Creación')
